2f07b33595b5e9fd7afd7c8612527c092e7a3505
Louie John Manuel Lauro <burshiki@gmail.com>
2022-09-15 14:27:57 +0800

    add login

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 
-Route::post('register', [AuthController::class, 'register']);
+Route::middleware(['cors'])->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
